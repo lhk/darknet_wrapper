a2186b3483950e2235f11cae8a0c1ad317b51964
Lars Henning Klein <lars.klein@rwth-aachen.de>
2017-06-20 22:04:42 +0200

    moving the code over

---
+++ install_opencv320.bash
@@ -0,0 +1,40 @@
+# Janis' opencv installation script
+
+set -e # exit if any command fails
+
+if [ -d "/opt/opencv320" ]; then
+    echo "Error: Directory /opt/opencv320 already exists."
+    exit
+fi
+
+sudo mkdir /opt/opencv320
+
+mkdir /tmp/opencvbuild320
+
+cd /tmp/opencvbuild320
+git clone https://github.com/opencv/opencv.git
+cd /tmp/opencvbuild320/opencv
+git checkout 3.2.0
+
+
+cd /tmp/opencvbuild320
+git clone https://github.com/opencv/opencv_contrib.git
+cd /tmp/opencvbuild320/opencv_contrib
+git checkout 3.2.0
+
+
+cd /tmp/opencvbuild320/opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/opt/opencv320 -D BUILD_EXAMPLES=ON -D CUDA_GENERATION=Auto -DOPENCV_EXTRA_MODULES_PATH=/tmp/opencvbuild320/opencv_contrib/modules ..
+
+
+read -p "Please check the cmake output. [Y] to continue: " -n 1 -r
+echo  # new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    make -j8
+    sudo make -j8 install
+    rm -rf /tmp/opencvbuild320
+fi
--- main.cpp
+++ main.cpp
@@ -1,3 +1,58 @@
+#include "darknet_wrapper/wrapper_cpp.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
 int main(){
+
+    char* cfg_path = "test/tiny-yolo-voc.cfg";
+    char* weights_path = "test/tiny-yolo-voc.weights";
+
+    // apply the detector to an image file
+    char* filename="dog.jpg";
+
+    float thresh = 0.4;
+    int hits;
+    box** outboxes = new box*;
+    float** outprobs = new float*;
+    int** outclasses = new int*;
+
+    Darknet::getInstance(cfg_path, weights_path).darknet_detect_file(filename, thresh, &hits, outboxes, outprobs, outclasses);
+
+    // apply the detector to an opencv mat
+    cv::Mat3b img= cv::imread("test/dog.jpg", cv::IMREAD_UNCHANGED);
+    if(!img.data){
+        cout<<"couldn't load the test image"<<endl;
+    }
+
+    IplImage convertedImage = img;
+
+    Darknet::getInstance(cfg_path, weights_path).darknet_detect_img(&convertedImage, thresh, &hits, outboxes, outprobs, outclasses);
+
+    cout<<"hits: "<<hits;
+    for (int i = 0; i < hits; i++) {
+        box hitbox = (*outboxes)[i];
+        int label = (*outclasses)[i];
+        float prob = (*outprobs)[i];
+
+        // calculate coordinates of bounding box
+        // yolo returns the center of the bbox
+        float x, y, w, h;
+        x = hitbox.x - hitbox.w / 2;
+        y = hitbox.y - hitbox.h / 2;
+        w = hitbox.w;
+        h = hitbox.h;
+
+        cv::rectangle(img, {x,y}, {x+w, y+h}, {0,0,255}, 10);
+    }
+
+    cv::imshow("rectangles in output", img);
+    cv::waitKey(0);
+
+    free((*outboxes));
+    free((*outprobs));
+    free((*outclasses));
+
     return 0;
 }
